829bfa7602036a89559b0a3df9d894be49b5b3b3
AryanAnsh2019 <52231066+AryanAnsh2019@users.noreply.github.com>
2021-09-21 20:00:10 +0530

    Styled the Detect page

--- src/Detect.js
+++ src/Detect.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { francAll } from "franc";
 import langs from "langs";
+import "./css/Detect.css";
 
 function Detect() {
   const [text, setText] = useState("");
@@ -16,33 +17,56 @@ function Detect() {
         .map((r) => (langs.where("3", r[0]) ? langs.where("3", r[0]).name : ""))
         .filter((e) => e)
     );
-    console.log(typeof results);
     setText("");
   }
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Put in text to detect"
-          value={text}
-          onChange={handleChange}
-        />
-      </form>
-      {results.length ? (
-        <>
-          <h6>Most Probable Languages</h6>
-          <ul>
-            {results.map((r) => (
-              <li key={r}>{r}</li>
-            ))}
-          </ul>
-        </>
-      ) : typeof results === "object" ? (
-        "Sorry! We were not able to detect the language of the above text..."
-      ) : (
-        ""
-      )}
+    <div className="Detect">
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Put in text to detect"
+            value={text}
+            onChange={handleChange}
+          />
+        </form>
+        <p>
+          We are easily confused on small samples. Make sure to pass it big
+          documents to get reliable results.
+        </p>
+      </div>
+      <div className="Detect-results">
+        {results.length ? (
+          <>
+            <h5>Most Probable Languages</h5>
+            <ul>
+              {results.map((r) => (
+                <li key={r}>{r}</li>
+              ))}
+            </ul>
+          </>
+        ) : typeof results === "object" ? (
+          <>
+            <p className="error">
+              Sorry! We were not able to detect the language of the above
+              text...
+            </p>
+            <div class="container">
+              <div class="tear"></div>
+              <div class="tear2"></div>
+              <div class="face">
+                <div class="eyebrow">︶</div>
+                <div class="eyebrow">︶</div>
+                <div class="eye"></div>
+                <div class="eye"></div>
+                <div class="mouth"></div>
+              </div>
+            </div>
+          </>
+        ) : (
+          ""
+        )}
+      </div>
     </div>
   );
 }
