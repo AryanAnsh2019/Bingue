6e588f27f03c7ba1b98384b1815e1417239a16d1
AryanAnsh2019 <52231066+AryanAnsh2019@users.noreply.github.com>
2021-09-21 10:26:43 +0530

    Added basic router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import Page from "./Page";
 import "./css/App.css";
-import Home from './Home'
+import Home from "./Home";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Page>
-        <Home/>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/translate" exact>
+            <h1>translate</h1>
+          </Route>
+          <Route path="/detect" exact>
+            <h1>detect</h1>
+          </Route>
+        </Switch>
       </Page>
     </div>
   );
